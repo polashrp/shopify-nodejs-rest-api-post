2e09c228e55c2a8917c52746cabe535a3f5a82cb
Habib Software Engineer <polashrp2640@gmail.com>
2022-02-13 20:17:34 +0600

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,47 @@
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import ClientRouter from '../components/ClientRouter';
+import { Provider } from '@shopify/app-bridge-react';
+import Cookies from "js-cookie";
+import '@shopify/polaris/styles.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include',
+  },
+});
+
+class MyApp extends App {
+
+  static async getInitialProps(server) {
+    const shopOrigin = server.ctx.query.shop;
+    return { shopOrigin };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Provider config={config}>
+        <ClientRouter />
+          <AppProvider>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
+          </AppProvider>
+        </Provider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
